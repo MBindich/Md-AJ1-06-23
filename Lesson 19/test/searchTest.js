c2142a611a861aeae147840c30b360df4bd54a8a
Mikhail Bindich <bindich.m@gmail.com>
2023-07-28 17:18:50 +0300

    Homework lesson 19 done

---
+++ Lesson 19/test/searchTest.js
@@ -0,0 +1,12 @@
+const {expect} = require('chai')
+
+describe('Webdriver.io search test', function () {
+    it('Should get relevant search result', async () => {
+        await browser.url('https://webdriver.io/');
+        await $('//button[@class="DocSearch DocSearch-Button"]').click();
+        await $('//*[@id="docsearch-input"]').addValue('get');
+        await $('//*[@id="docsearch-item-0"]/a').click();
+        const link = await browser.getUrl(); 
+        expect(await link.includes("get")).to.equal(true);
+    })
+});
